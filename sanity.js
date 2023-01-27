ffa2994cf837a959f28486bfc55321bd07c2f779
Deepak Singh <singhdeepak5219@gmail.com>
2023-01-27 19:09:18 +0530

    basic fixes

--- sanity.js
+++ sanity.js
@@ -4,11 +4,10 @@ import imageUrlBuilder from '@sanity/image-url';
 const client = sanityClient({
   projectId: 'bvdz90sj',
   dataset: 'production',
-  apiVersion: '2022-03-25', // use current UTC date - see "specifying API version"!
-  useCdn: true, // `false` if you want to ensure fresh data
+  apiVersion: '2021-10-21', 
+  useCdn: true, 
 })
 
 const builder = imageUrlBuilder(client);
 export const urlFor = (source) => builder.image(source);
-
 export default client;
